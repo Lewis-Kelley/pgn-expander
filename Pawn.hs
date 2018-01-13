c4bdba83c47ed39821876e145be859b7bc786d84
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-01-12 21:59:08 -0500

    Fixed Pawn to use proper Cell type.

--- Pawn.hs
+++ Pawn.hs
@@ -3,13 +3,13 @@ module Pawn where
 import Board
 import Types
 
-validPawnMove :: GameState -> Color -> (Int, Int) -> (Int, Int) -> Bool
+validPawnMove :: GameState -> Color -> Cell -> Cell -> Bool
 validPawnMove state color origin dest =
   validDoubleMove state color origin dest
   || validSingleMove state color origin dest
   || validTakingMove state color origin dest
 
-validDoubleMove :: GameState -> Color -> (Int, Int) -> (Int, Int) -> Bool
+validDoubleMove :: GameState -> Color -> Cell -> Cell -> Bool
 validDoubleMove state color (originCol, originRow) (destCol, destRow)
   | color == White = originCol == destCol
                      && destRow - originRow == 2
@@ -22,7 +22,7 @@ validDoubleMove state color (originCol, originRow) (destCol, destRow)
                      && pieceColorAtPosIs state (destCol, destRow) Nothing
                      && pieceColorAtPosIs state (destCol, destRow + 1) Nothing
 
-validSingleMove :: GameState -> Color -> (Int, Int) -> (Int, Int) -> Bool
+validSingleMove :: GameState -> Color -> Cell -> Cell -> Bool
 validSingleMove state color (originCol, originRow) (destCol, destRow)
   | color == White = originCol == destCol
                      && destRow - originRow == 1
