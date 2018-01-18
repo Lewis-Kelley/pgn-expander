8df18ffc1e5511b25c4a52fd5c78ea47c2e56092
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-01-17 20:12:18 -0500

    Started outputting csv.

--- OutputFormatting.hs
+++ OutputFormatting.hs
@@ -2,30 +2,46 @@ module OutputFormatting where
 
 import Types
 
-formatMoves :: [Move] -> [String]
-formatMoves = map formatMove
+-- |GameID|Outcome|White Name|Black Name|White ELO|Black ELO|Date|...
+-- |GameID|Turn|Ply No.|Piece|Source|Dest|CapturedPiece|PromotionPiece|CheckState|CastleSide|
 
-formatMove :: Move -> String
-formatMove (BasicMove piece origin destination promotion checkState) =
-  formatColoredPiece piece ++ " " ++
-  formatCell origin ++ "->" ++
-  formatCell destination ++ " " ++
-  formatPromotion promotion ++ " " ++
-  formatCheckState checkState ++ "\n"
-formatMove (TakingMove piece takenPiece origin destination promotion checkState) =
-  formatColoredPiece piece ++ " " ++ "takes " ++
-  formatColoredPiece takenPiece ++ " " ++
-  formatCell origin ++ "->" ++
-  formatCell destination ++ " " ++
-  formatPromotion promotion ++ " " ++
-  formatCheckState checkState ++ "\n"
-formatMove (CastleMove side checkState) =
-  "castle " ++ formatCastleSide side ++ " " ++
-  formatCheckState checkState ++ "\n"
+formatMoves :: [Move] -> [String]
+formatMoves = map formatMoveType
+
+formatMoveType :: Move -> String
+formatMoveType (BasicMove piece origin destination
+                 promotion checkState) =
+  formatMove piece origin destination Nothing
+  promotion checkState Nothing
+formatMoveType (TakingMove piece takenPiece origin destination
+                 promotion checkState) =
+  formatMove piece origin destination (Just takenPiece)
+  promotion checkState Nothing
+--FIXME Once we have turn information
+formatMoveType (CastleMove KingSide checkState) =
+  formatMove (ColoredPiece White King) (4, 0) (6, 0) Nothing
+  Nothing checkState (Just KingSide)
+formatMoveType (CastleMove QueenSide checkState) =
+  formatMove (ColoredPiece White King) (4, 0) (2, 0) Nothing
+  Nothing checkState (Just QueenSide)
+
+formatMove :: ColoredPiece -> Cell -> Cell -> Maybe ColoredPiece
+  -> Promotion -> CheckState -> Maybe CastleSide -> String
+formatMove movedPiece origin destination takenPiece promotion checkState castleSide =
+  -- GameID
+  -- Turn
+  -- Ply
+  formatColoredPiece movedPiece ++ "," ++
+  formatCell origin ++ "," ++
+  formatCell destination ++ "," ++
+  formatTakenPiece takenPiece ++ "," ++
+  formatPromotion promotion ++ "," ++
+  formatCheckState checkState ++ "," ++
+  formatCastleSide castleSide ++ "\n"
 
 formatCell :: Cell -> String
 formatCell (row, col) =
-  "(" ++ formatRow row ++ "," ++ formatCol col ++ ")"
+  formatRow row ++ "," ++ formatCol col
 
 formatRow :: Int -> String
 formatRow = show
@@ -33,10 +49,25 @@ formatRow = show
 formatCol :: Int -> String
 formatCol = show
 
+formatTakenPiece :: Maybe ColoredPiece -> String
+formatTakenPiece Nothing = ""
+formatTakenPiece (Just NoPiece) = ""
+formatTakenPiece (Just piece@(ColoredPiece _ _)) = formatColoredPiece piece
+
 formatColoredPiece :: ColoredPiece -> String
 formatColoredPiece NoPiece = ""
-formatColoredPiece (ColoredPiece color piece) =
-  formatColor color ++ " " ++ formatPiece piece
+formatColoredPiece (ColoredPiece White Pawn) = "P"
+formatColoredPiece (ColoredPiece Black Pawn) = "p"
+formatColoredPiece (ColoredPiece White Rook) = "R"
+formatColoredPiece (ColoredPiece Black Rook) = "r"
+formatColoredPiece (ColoredPiece White Knight) = "N"
+formatColoredPiece (ColoredPiece Black Knight) = "n"
+formatColoredPiece (ColoredPiece White Bishop) = "B"
+formatColoredPiece (ColoredPiece Black Bishop) = "b"
+formatColoredPiece (ColoredPiece White Queen) = "Q"
+formatColoredPiece (ColoredPiece Black Queen) = "q"
+formatColoredPiece (ColoredPiece White King) = "K"
+formatColoredPiece (ColoredPiece Black King) = "k"
 
 formatColor :: Color -> String
 formatColor White = "white"
@@ -51,14 +82,15 @@ formatPiece Queen = "queen"
 formatPiece King = "king"
 
 formatPromotion :: Promotion -> String
-formatPromotion Nothing = "-"
-formatPromotion (Just piece) = "^" ++ formatPiece piece
+formatPromotion Nothing = ""
+formatPromotion (Just piece) = formatPiece piece
 
 formatCheckState :: CheckState -> String
-formatCheckState NoCheck = "-"
+formatCheckState NoCheck = ""
 formatCheckState Check = "check"
 formatCheckState CheckMate = "checkmate"
 
-formatCastleSide :: CastleSide -> String
-formatCastleSide QueenSide = "queenside"
-formatCastleSide KingSide = "kingside"
+formatCastleSide :: Maybe CastleSide -> String
+formatCastleSide Nothing = ""
+formatCastleSide (Just QueenSide) = "q"
+formatCastleSide (Just KingSide) = "k"
